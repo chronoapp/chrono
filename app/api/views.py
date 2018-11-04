ad93fdea7bad682f45a19db1fbed4dd8b97e99c7
Winston Wu <winxton@gmail.com>
2018-11-04 18:43:07 -0500

    Setup build, some mocks for client

--- app/api/views.py
+++ app/api/views.py
@@ -8,9 +8,17 @@ from .models import User, Label
 DEFAULT_CATEGORIES = ['eat', 'entertainment', 'transportation', 'work', 'exercise',
               'social', 'reading', 'education', 'tourism', 'chore', 'event', 'sleep']
 
+
 @app.route('/')
 async def getHealthCheck(request):
     return response.json({'data': {'healthcheck': 'OK'}})
+
+
+@app.route('/users/<userId>/stats')
+async def getUserStats(request, userId):
+    # TODO: Gets summary of time trends
+    return response.json({})
+
 
 def createUser(username):
     with scoped_session() as session:    
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,115 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
+import { BarChart, PieChart, Pie, Bar, XAxis, YAxis,
+  CartesianGrid, Tooltip, Legend, Label, LabelList} from 'recharts';
 
-class App extends Component {
+interface State {
+  dropdownActive: boolean
+}
+
+class App extends Component<{}, State> {
+  data: any
+  options: any
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      dropdownActive: false
+    }
+
+    this.data = [
+      {name: 'Eating', hours: 5, fill: '#106596'},
+      {name: 'Sleeping', hours: 15, fill: '#1d7c91'},
+      {name: 'Working', hours: 22, fill: '#1d7c91'},
+      {name: 'Browsing', hours: 11 },
+      {name: 'Meetups', hours: 5 },
+    ];
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  renderCustomLabel(data: any) {;
+    return data.name;
+  }
+
+  toggleDropdown() {
+    const dropdownActive = !this.state.dropdownActive;
+    this.setState({dropdownActive: dropdownActive});
+  }
+
+  renderDropdown() {
+    return (
+      <div onClick={this.toggleDropdown}
+          className={`dropdown is-clearfix ${this.state.dropdownActive ? 'is-active': ''}`}>
+        <div className="dropdown-trigger">
+          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Last 7 days</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            <a href="#" className="dropdown-item">
+              Dropdown item
+            </a>
+            <a className="dropdown-item">
+              Other dropdown item
+            </a>
+            <a href="#" className="dropdown-item is-active">
+              Active dropdown item
+            </a>
+            <a href="#" className="dropdown-item">
+              Other dropdown item
+            </a>
+            <hr className="dropdown-divider"/>
+            <a href="#" className="dropdown-item">
+              With a divider
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="section">
+          <div className="container">
+            <h1 className="title">
+              Time Trends
+            </h1>
+            <p className="subtitle">
+              Activities over time.
+            </p>
+            <div className="notification columns">
+                { this.renderDropdown() }
+            </div>
+          </div>
+        </section>
+
+        <section>
+            <div className="container">
+              <BarChart layout={'vertical'} width={600} height={300} data={this.data}
+                  margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis type="number"/>
+                <YAxis dataKey="name" type="category"/>
+                <Tooltip/>
+                <Legend />
+                <Bar dataKey="hours" />
+              </BarChart>
+
+              <PieChart width={400} height={400}>
+                <Pie data={this.data} dataKey="hours" nameKey="name" label={this.renderCustomLabel} outerRadius={80}>
+                </Pie>
+                <Tooltip/>
+              </PieChart>
+            </div>
+          </section>
       </div>
     );
   }
