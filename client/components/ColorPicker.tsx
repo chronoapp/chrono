d153bd071761aca6b6bf410f3e3c7e5b53f35915
Winston Wu <winxton@gmail.com>
2020-04-08 11:15:35 -0400

    replace react class with functions

--- client/components/ColorPicker.tsx
+++ client/components/ColorPicker.tsx
@@ -1,43 +1,40 @@
-import * as React from 'react';
+import React from 'react';
 import { getSortedLabelColors } from '../models/LabelColors';
 
 interface IProps {
     onSelectLabelColor: (string) => void;
 }
 
-export default class ColorPicker extends React.PureComponent<IProps, {}> {
-    private labelColors = getSortedLabelColors()
+export default function ColorPicker(props: IProps) {
 
-    render() {
-        const colors = this.labelColors.map(x => x.hex);
+    const colors = getSortedLabelColors().map(x => x.hex);
 
-        return (
-            <div className="dropdown-menu" id="dropdown-menu" role="menu" style={{maxWidth: '12em'}}>
-                <div className="dropdown-content">
-                    <div className="columns" style={{paddingLeft: '1.5em', marginBottom: 0}}>
-                        {colors.slice(0, colors.length/2).map(color => {
-                            return (
-                                <div key={color} className="column is-1">
-                                    <div style={{backgroundColor: color}}
-                                        onClick={() => this.props.onSelectLabelColor(color)}
-                                        className="event-label event-label--hoverable"></div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className="columns" style={{paddingLeft: '1.5em'}}>
-                        {colors.slice(6, colors.length).map(color => {
-                            return (
-                                <div key={color} className="column is-1">
-                                    <div style={{backgroundColor: color}}
-                                        onClick={() => this.props.onSelectLabelColor(color)}
-                                        className="event-label event-label--hoverable"></div>
-                                </div>
-                            )
-                        })}
-                    </div>
+    return (
+        <div className="dropdown-menu" id="dropdown-menu" role="menu" style={{maxWidth: '12em'}}>
+            <div className="dropdown-content">
+                <div className="columns" style={{paddingLeft: '1.5em', marginBottom: 0}}>
+                    {colors.slice(0, colors.length/2).map(color => {
+                        return (
+                            <div key={color} className="column is-1">
+                                <div style={{backgroundColor: color}}
+                                    onClick={() => props.onSelectLabelColor(color)}
+                                    className="event-label event-label--hoverable"></div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="columns" style={{paddingLeft: '1.5em'}}>
+                    {colors.slice(6, colors.length).map(color => {
+                        return (
+                            <div key={color} className="column is-1">
+                                <div style={{backgroundColor: color}}
+                                    onClick={() => props.onSelectLabelColor(color)}
+                                    className="event-label event-label--hoverable"></div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
--- client/components/LabelPanel.tsx
+++ client/components/LabelPanel.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
+
 import Icon from '@mdi/react'
 import { mdiDotsHorizontal } from '@mdi/js'
 
 import Hoverable from '../core/Hoverable';
 import ColorPicker from './ColorPicker';
 import { Label } from '../models/Label';
-import { getSortedLabelColors } from '../models/LabelColors';
 
 
 interface IProps {
@@ -13,65 +14,43 @@ interface IProps {
     updateLabel: (label: Label) => void;
 }
 
-interface IState {
-    selectedLabelKeyColor: string | null
-}
-
 /**
  * Panel with a list of labels.
  */
-export default class LabelPanel extends React.Component<IProps, IState> {
-    private labelColors = getSortedLabelColors()
+export default function LabelPanel(props: IProps) {
+    const [selectedLabelKeyColor, setSelectedLabelKeyColor] = useState('')
 
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            selectedLabelKeyColor: null
-        }
-        this.getLabel = this.getLabel.bind(this);
-        this.onClickLabelColor = this.onClickLabelColor.bind(this);
-    }
-
-    render() {
-        return (
-            <nav className="panel" style={{marginBottom: '0.5rem'}}>
-                <p className="panel-heading">Projects</p>
-                {this.props.labels.map(this.getLabel)}
-            </nav>
-        );
-    }
-
-    private toggleLabelKeyColor(labelKey: string) {
-        if (labelKey == this.state.selectedLabelKeyColor) {
-            this.setState({selectedLabelKeyColor: null})
+    function toggleLabelKeyColor(labelKey: string) {
+        if (labelKey == selectedLabelKeyColor) {
+            setSelectedLabelKeyColor('');
         } else {
-            this.setState({selectedLabelKeyColor: labelKey})
+            setSelectedLabelKeyColor(labelKey);
         }
     }
 
-    private onClickLabelColor(labelColor: string) {
-        const selectedLabel = this.props.labels.find(
-            label => label.key == this.state.selectedLabelKeyColor);
+    function onClickLabelColor(labelColor: string) {
+        const selectedLabel = props.labels.find(
+            label => label.key == selectedLabelKeyColor);
         if (selectedLabel) {
             selectedLabel.color_hex = labelColor;
-            this.props.updateLabel(selectedLabel);
+            props.updateLabel(selectedLabel);
         }
-        this.setState({selectedLabelKeyColor: null});
+        setSelectedLabelKeyColor('');
     }
 
-    private getLabel(label: Label) {
+    function getLabel(label: Label) {
         return (
             <Hoverable key={label.id}>
                 {(isMouseInside, mouseEnter, mouseLeave) => (
                 <a onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} key={label.key}
                     className={`panel-block ${false ? 'is-active' : ''}`}>
-                    <div className={`dropdown ${label.key === this.state.selectedLabelKeyColor ? 'is-active' : ''}`}>
+                    <div className={`dropdown ${label.key === selectedLabelKeyColor ? 'is-active' : ''}`}>
                         <div
-                            onClick={_ => this.toggleLabelKeyColor(label.key)}
+                            onClick={_ => toggleLabelKeyColor(label.key)}
                             style={{backgroundColor: label.color_hex}}
                             className="event-label event-label--hoverable dropdown-trigger"></div>
-                        {label.key === this.state.selectedLabelKeyColor ?
-                            <ColorPicker onSelectLabelColor={this.onClickLabelColor}/> : null}
+                        {label.key === selectedLabelKeyColor ?
+                            <ColorPicker onSelectLabelColor={onClickLabelColor}/> : null}
                     </div>
                     <span style={{marginLeft: 10}}>{label.title}</span>
                     {isMouseInside ? <Icon path={mdiDotsHorizontal}
@@ -84,4 +63,11 @@ export default class LabelPanel extends React.Component<IProps, IState> {
             </Hoverable>
         )
     }
+
+    return (
+        <nav className="panel" style={{marginBottom: '0.5rem'}}>
+            <p className="panel-heading">Projects</p>
+            {props.labels.map(getLabel)}
+        </nav>
+    );
 }
