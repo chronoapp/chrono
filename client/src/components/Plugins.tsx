f7e6a7588c83ecfef340d8e829f16b8ded9adb3c
winxton <winxton@gmail.com>
2021-02-21 21:00:30 -0500

    Replace plugins css.

--- client/src/components/Plugins.tsx
+++ client/src/components/Plugins.tsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import Link from 'next/link'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Text, Flex } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import clsx from 'clsx'
 import { BsBarChartFill } from 'react-icons/bs'
 
 export default function Plugins() {
   const router = useRouter()
+  const isTrendsActive = router.pathname == '/trends'
 
   return (
-    <div className="cal-plugins is-flex is-justify-content-center">
-      <div
-        className={clsx(
-          'cal-plugin-icon mt-2 is-flex-direction-column',
-          router.pathname == '/trends' && 'cal-plugin-active'
-        )}
-      >
-        <Link href="/trends">
-          <Box>
-            <Flex justifyContent="center">
-              <BsBarChartFill size={25} className="has-text-grey-light" />
-            </Flex>
-            <span className="cal-plugin-text is-size-7 has-text-grey">Trends</span>
-          </Box>
-        </Link>
-      </div>
-    </div>
+    <Flex className="cal-plugins" justifyContent="center" pt="2">
+      <Link href="/trends">
+        <Box
+          p="1"
+          lineHeight="0.8"
+          height="max-content"
+          borderRadius="md"
+          className={clsx('cal-plugin-icon', isTrendsActive && 'cal-plugin-active')}
+        >
+          <Flex direction="column" align="center" color="gray.400">
+            <BsBarChartFill size={25} />
+            <Text
+              fontWeight="normal"
+              fontSize="xs"
+              color={`${isTrendsActive ? 'gray.600' : 'gray.500'}`}
+              mt="1"
+            >
+              Trends
+            </Text>
+          </Flex>
+        </Box>
+      </Link>
+    </Flex>
   )
 }
