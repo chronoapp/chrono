8d2d8ed24c8c6e50bb670f36e851956263d48647
Winston Wu <winxton@gmail.com>
2021-11-16 07:45:02 -0500

    Add photo_url to Contact.

--- server/app/db/models/contact.py
+++ server/app/db/models/contact.py
@@ -10,7 +10,7 @@ from app.db.base_class import Base
 class Contact(Base):
     __tablename__ = 'contact'
 
-    id = Column(String(255), unique=True, primary_key=True, default=shortuuid.uuid)
+    id = Column(String(255), primary_key=True, default=shortuuid.uuid)
     google_id = Column(String(255), unique=True, nullable=True)
 
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
@@ -19,15 +19,18 @@ class Contact(Base):
     first_name = Column(String(255), nullable=True)
     last_name = Column(String(255), nullable=True)
     email_address = Column(String(255), nullable=True)
+    photo_url = Column(String(255), nullable=True)
 
     def __init__(
         self,
         first_name: Optional[str],
         last_name: Optional[str],
         email_address: Optional[str],
+        photo_url: Optional[str],
         google_id: Optional[str],
     ):
         self.first_name = first_name
         self.last_name = last_name
         self.email_address = email_address
+        self.photo_url = photo_url
         self.google_id = google_id
