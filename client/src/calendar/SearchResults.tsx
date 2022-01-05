ea2abd7b7030f354af69ee3e5ac2428365c84a5a
Winston Wu <winxton@gmail.com>
2022-01-05 17:24:32 -0500

    Search: sorted events, enable event editing.

--- client/src/calendar/SearchResults.tsx
+++ client/src/calendar/SearchResults.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Flex, Box, Text } from '@chakra-ui/react'
+import { Portal, Popover, PopoverTrigger, PopoverContent, PopoverArrow } from '@chakra-ui/react'
+
+import { EventActionContext, EventActionContextType } from '@/calendar/EventActionContext'
+import EventPopover from '@/calendar/event-edit/EventPopover'
+import { sortEvents } from '@/calendar/utils/eventLevels'
 
 import { CalendarsContext, CalendarsContextType } from '@/contexts/CalendarsContext'
-import { EventActionContext, EventActionContextType } from '@/calendar/EventActionContext'
 import { format, timeRangeFormat } from '@/util/localizer'
 import { LabelTag } from '@/components/LabelTag'
-
 import Event from '@/models/Event'
-import * as API from '@/util/Api'
 
 interface IProps {
   search: string
@@ -16,47 +18,84 @@ interface IProps {
 
 function EventItem(props: { event: Event }) {
   const calendarContext = React.useContext<CalendarsContextType>(CalendarsContext)
+  const eventActionContext = React.useContext<EventActionContextType>(EventActionContext)
+
   const dateDisplay = format(props.event.start, 'D MMM YYYY, ddd')
   const color = calendarContext.getCalendarColor(props.event.calendar_id)
 
-  return (
-    <Flex
-      alignItems={'center'}
-      borderBottom="1px solid"
-      borderColor={'gray.200'}
-      pl="5"
-      pt="1"
-      pb="1"
-    >
-      <Box w="8em" flexShrink={0}>
-        <Text fontSize="sm" align="left">
-          {dateDisplay}
-        </Text>
-      </Box>
-      <Flex direction={'column'}>
-        <Flex alignItems={'center'}>
-          <Box pl="1" bgColor={color} w="1em" h="1em" borderRadius={'5'} flexShrink={0}></Box>
-          <Text pl="2" fontSize="sm" textAlign={'left'}>
-            {props.event.title_short}
+  const editingEvent = eventActionContext.eventState.editingEvent
+  const isEditing = editingEvent?.id === props.event.id && editingEvent?.editMode !== 'FULL_EDIT'
+
+  function onSelectEvent() {
+    eventActionContext?.eventDispatch({
+      type: 'INIT_EDIT_EVENT',
+      payload: { event: props.event },
+    })
+  }
+
+  function renderEvent() {
+    return (
+      <Flex
+        alignItems={'center'}
+        borderBottom="1px solid"
+        borderColor={'gray.200'}
+        pl="5"
+        pt="1"
+        pb="1"
+        _hover={{
+          background: 'gray.100',
+          cursor: 'pointer',
+        }}
+        onClick={onSelectEvent}
+      >
+        <Box w="8em" flexShrink={0}>
+          <Text fontSize="sm" align="left">
+            {dateDisplay}
           </Text>
-          <Flex pl="2" alignItems={'center'}>
-            {props.event.labels.map((label) => {
-              return <LabelTag key={`${props.event.id}-${label.id}`} label={label} />
-            })}
+        </Box>
+        <Flex direction={'column'}>
+          <Flex alignItems={'center'}>
+            <Box pl="1" bgColor={color} w="1em" h="1em" borderRadius={'5'} flexShrink={0}></Box>
+            <Text pl="2" fontSize="sm" textAlign={'left'}>
+              {props.event.title_short}
+            </Text>
+            <Flex pl="2" alignItems={'center'}>
+              {props.event.labels.map((label) => {
+                return <LabelTag key={`${props.event.id}-${label.id}`} label={label} />
+              })}
+            </Flex>
+          </Flex>
+          <Flex>
+            <Text fontSize="xs">{timeRangeFormat(props.event.start, props.event.end)}</Text>
           </Flex>
         </Flex>
-        <Flex>
-          <Text fontSize="xs">{timeRangeFormat(props.event.start, props.event.end)}</Text>
-        </Flex>
       </Flex>
-    </Flex>
-  )
+    )
+  }
+
+  if (isEditing) {
+    return (
+      <Popover isOpen={true}>
+        <PopoverTrigger>{renderEvent()}</PopoverTrigger>
+        <Portal>
+          <PopoverContent w="25em">
+            <PopoverArrow />
+            <EventPopover event={props.event} />
+          </PopoverContent>
+        </Portal>
+      </Popover>
+    )
+  } else {
+    return renderEvent()
+  }
 }
 
 export default function SearchResults(props: IProps) {
+  const sortedEvents = props.events.sort((a, b) => -sortEvents(a, b))
+
   return (
     <Box w="100%" height="calc(100vh - 3.25rem)" overflow="auto">
-      {props.events.map((event, idx, arr) => (
+      {sortedEvents.map((event, idx, arr) => (
         <EventItem key={idx} event={event} />
       ))}
     </Box>
