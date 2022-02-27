b3d96e274525b6c87b239e373439613379637ee7
Winston Wu <winxton@gmail.com>
2022-02-27 20:50:16 +0800

    Fix event search query.

--- server/app/db/sql/event_search.py
+++ server/app/db/sql/event_search.py
@@ -4,9 +4,11 @@ EVENT_SEARCH_QUERY = """
             setweight(to_tsvector('english', event.title), 'A') ||
             setweight(to_tsvector('english', coalesce(event.description, '')), 'B') as doc
         FROM event
-        WHERE event.user_id = :userId
+        INNER JOIN event_calendar ON event_calendar.event_id = event.id
+        INNER JOIN user_calendar ON event_calendar.calendar_id = user_calendar.id
         AND event.recurrences is NULL
         AND event.status != 'deleted'
+        AND user_calendar.user_id = :userId
     ) search
     WHERE search.doc @@ to_tsquery(:query || ':*')
     ORDER BY ts_rank(search.doc, to_tsquery(:query || ':*')) DESC
