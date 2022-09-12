ef7d46b6ab2d3358a476c2cd1b1c7a5fd22e2bd1
Winston Wu <winxton@gmail.com>
2022-09-13 04:32:35 +0700

    Search includes both recurring and single events.

--- server/app/db/sql/event_search_recurring.py
+++ server/app/db/sql/event_search_recurring.py
@@ -1,11 +1,11 @@
-EVENT_SEARCH_QUERY = """
+RECURRING_EVENT_SEARCH_QUERY = """
     SELECT id FROM (
         SELECT event.*,
             setweight(to_tsvector('english', event.title), 'A') as doc
         FROM event
         INNER JOIN user_calendar
         ON event.calendar_id = user_calendar.id
-        AND event.recurrences is NULL
+        AND event.recurrences is NOT NULL
         AND event.status != 'deleted'
         AND user_calendar.user_id = :userId
     ) search
