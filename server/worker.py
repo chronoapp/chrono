a2e3e700910ab68123731fb7d121e89f6598f574
Winston Wu <winxton@gmail.com>
2023-07-08 02:41:19 -0400

    Split entrypoint scripts by worker and web.

--- server/worker.py
+++ server/worker.py
@@ -1,6 +1,9 @@
 import dramatiq
+import os
 from dramatiq.brokers.redis import RedisBroker
 
-redisBroker = RedisBroker(host="redis")
+redisUrl = os.getenv('REDIS_URL', 'redis://redis:6379')
+redisBroker = RedisBroker(url=redisUrl)
+
 dramatiq.set_broker(redisBroker)
 dramatiq.set_encoder(dramatiq.PickleEncoder)
