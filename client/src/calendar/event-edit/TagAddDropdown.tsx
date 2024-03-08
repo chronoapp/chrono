67140a6ad0b2837dfc767cd9ef30630707e90d61
Tommy Yang <tommy_yang621@hotmail.com>
2024-03-08 13:42:09 -0500

    change function into a reuseable component

---
+++ client/src/calendar/event-edit/TagAddDropdown.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Menu, MenuButton, MenuList, Button, Flex } from '@chakra-ui/react'
+import { FiPlus } from 'react-icons/fi'
+import LabelTree from '@/components/LabelTree'
+import produce from 'immer'
+
+const TagDropdown = ({ eventFields, setEventFields }) => {
+  return (
+    <Menu isLazy>
+      {({ onClose }) => (
+        <>
+          <MenuButton
+            borderRadius="xs"
+            size="sm"
+            fontWeight="normal"
+            fontSize="sm"
+            as={Button}
+            variant="link"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Flex align="center">
+              <FiPlus /> add tag
+            </Flex>
+          </MenuButton>
+          <MenuList pl="1">
+            <LabelTree
+              allowEdit={false}
+              onSelect={(label) => {
+                const updatedLabels = produce(eventFields.labels, (draft) => {
+                  if (!draft.find((l) => l.id === label.id)) {
+                    draft.push(label)
+                  }
+                })
+
+                setEventFields({ ...eventFields, labels: updatedLabels })
+                onClose()
+              }}
+            />
+          </MenuList>
+        </>
+      )}
+    </Menu>
+  )
+}
+
+export default TagDropdown
