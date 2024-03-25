e7da48bb17b0008674fbccd6f1d44caf4f566c2b
Tommy Yang <tommy_yang621@hotmail.com>
2024-03-25 15:53:48 -0400

    wip

--- client/src/calendar/TimezoneLabel.tsx
+++ client/src/calendar/TimezoneLabel.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { DateTime } from 'luxon';
+import React from 'react'
+import { DateTime } from 'luxon'
+import { Box } from '@chakra-ui/react'
 
 const TimezoneLabel = () => {
-  // Get the local timezone offset in hours
-  const timezoneOffset = DateTime.local().offset / 60;
+  const timezoneOffset = DateTime.local().offset / 60
 
   // Format the timezone as GMT+/-X
   const formatTimezone = (offset) => {
     // If the offset is 0, it means it's GMT
-    if (offset === 0) return 'GMT';
-    // Determine the sign and format accordingly
-    const sign = offset < 0 ? '-' : '+';
-    return `GMT${sign}${Math.abs(offset)}`;
-  };
+    if (offset === 0) return 'GMT'
 
-  return (
-    <div>
-      {formatTimezone(timezoneOffset)}
-    </div>
-  );
-};
+    const sign = offset < 0 ? '-' : '+'
+    return `GMT${sign}${Math.abs(offset)}`
+  }
 
-export default TimezoneLabel;
+  return <Box>{formatTimezone(timezoneOffset)}</Box>
+}
+
+export default TimezoneLabel
