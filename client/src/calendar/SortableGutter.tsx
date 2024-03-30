e3299400e52f4f9f8b5aeff2b198f43e2a75ee28
Tommy Yang <tommy_yang621@hotmail.com>
2024-03-29 21:36:03 -0400

    adjusted some code

--- client/src/calendar/SortableGutter.tsx
+++ client/src/calendar/SortableGutter.tsx
@@ -4,7 +4,9 @@ import { useSortable } from '@dnd-kit/sortable'
 import GutterContent from './GutterContent' // Make sure to import the child component
 
 export function SortableGutter({ id, gutterRef, slotMetrics }) {
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id })
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id,
+  })
 
   const style = {
     transform: CSS.Transform.toString(transform),
