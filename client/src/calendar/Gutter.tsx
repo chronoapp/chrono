acd46cda40c5f88f04b8bc63568309956dc117e1
Tommy Yang <tommy_yang621@hotmail.com>
2024-04-02 17:36:25 -0400

    wip

---
+++ client/src/calendar/Gutter.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { SortableContext, arrayMove, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import { SortableGutter } from './SortableGutter'
+import GutterContent from './GutterContent'
+import { DndContext, DragOverlay, closestCorners } from '@dnd-kit/core'
+import { IconButton, Flex } from '@chakra-ui/react'
+import { FiPlus } from 'react-icons/fi'
+
+const Gutter = ({ slotMetrics, gutterRef, gutters, addGutter, width, setGutters }) => {
+  const [activeId, setActiveId] = useState(null)
+
+  const getGutterPos = (id) => gutters.findIndex((gutter) => gutter.id === id)
+  function handleDragStart(event) {
+    const { active } = event
+    setActiveId(active.id)
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event
+    if (active.id !== over?.id) {
+      setGutters((gutters) => {
+        const originalPos = getGutterPos(active.id)
+        const newPos = getGutterPos(over.id)
+
+        return arrayMove(gutters, originalPos, newPos)
+      })
+    }
+  }
+
+  const restrictToXAxis = ({ transform }) => ({
+    ...transform,
+    y: 0, // Restrict movement to the x-axis by setting the y-coordinate to 0
+  })
+
+  function ToogleAdditionalTimezone({ addGutter }) {
+    return (
+      <IconButton
+        size={'xs'}
+        variant="ghost"
+        aria-label="adding additional timezones"
+        icon={<FiPlus />}
+        onClick={() => addGutter()}
+        width="4"
+      />
+    )
+  }
+
+  return (
+    <>
+      <Flex
+        width={width}
+        direction={'column'}
+        justifyContent={'flex-start'}
+        alignItems={'center'}
+        className="rbc-label cal-time-header-gutter"
+      >
+        <ToogleAdditionalTimezone addGutter={addGutter} />
+      </Flex>
+      <DndContext
+        collisionDetection={closestCorners}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        modifiers={[restrictToXAxis]}
+      >
+        <SortableContext
+          items={gutters.map((gutter) => gutter.id)}
+          strategy={horizontalListSortingStrategy}
+        >
+          {gutters.map((gutter) => (
+            <SortableGutter
+              key={gutter.id}
+              id={gutter.id}
+              slotMetrics={slotMetrics}
+              gutterRef={gutterRef}
+            />
+          ))}
+        </SortableContext>
+        <DragOverlay>
+          {activeId ? (
+            <div style={{ boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.2)' }}>
+              <GutterContent slotMetrics={slotMetrics} />
+            </div>
+          ) : null}
+        </DragOverlay>
+      </DndContext>
+    </>
+  )
+}
+
+export default Gutter
