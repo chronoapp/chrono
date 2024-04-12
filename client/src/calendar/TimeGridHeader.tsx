739069f7b9a701dcbf0c8180179538ddf3d72b43
Winston Wu <winxton@gmail.com>
2024-04-12 00:18:35 -0400

    Fix header size.

--- client/src/calendar/TimeGridHeader.tsx
+++ client/src/calendar/TimeGridHeader.tsx
@@ -77,12 +77,14 @@ function TimeGridHeader(props: IProps) {
           className={isToday ? 'cal-today' : ''}
         >
           <VStack spacing={1}>
-            <Box px={3} py={2}>
+            <Box>
               <Text
                 color={isToday ? 'white' : 'gray.600'}
                 fontWeight={'500'}
                 bg={isToday ? '#5c6bc0' : 'transparent'}
                 borderRadius={5}
+                px={1}
+                py={'0.5'}
                 fontSize="sm"
               >
                 {dayNumber}
