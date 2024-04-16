dd96b6b312e8f4ee9e539b97d897e33c34f4ae68
Winston Wu <winxton@gmail.com>
2024-04-16 03:48:57 -0400

    Add loading indicator.

--- client/src/routes/loading.tsx
+++ client/src/routes/loading.tsx
@@ -1,15 +1,20 @@
 import { Image, Box, Flex, Text } from '@chakra-ui/react'
-
+import { Oval } from 'react-loader-spinner'
 import ChronoLogo from '@/assets/chrono.svg'
 
 export function LoadingScreen() {
   return (
-    <Box position="fixed" top="30%" left="50%" transform="translate(-50%, -30%)">
+    <Box position="fixed" top="40%" left="50%" transform="translate(-50%, -40%)">
       <Flex alignItems="center" justifyContent="center" direction="column">
-        <Image src={ChronoLogo} alt="Chrono logo" boxSize={'5em'} />
-        <Text fontWeight={'medium'} fontSize={'sm'} color={'gray.600'} mt="1em">
-          Setting up your calendar..
-        </Text>
+        <Image src={ChronoLogo} alt="Chrono logo" boxSize={'5em'} mb="4" />
+        <Oval
+          visible={true}
+          height="30"
+          width="30"
+          strokeWidth="4"
+          color="#BDBDBD"
+          secondaryColor="#D9D9D9"
+        />
       </Flex>
     </Box>
   )
