28915645d7ad1a65a96159ea87f53a507fd5ebe0
Winston Wu <winxton@gmail.com>
2024-04-18 01:54:17 -0400

    Handle user not found in get_current_user.

--- server/app/api/utils/security.py
+++ server/app/api/utils/security.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from app.api.utils.db import get_db
 from app.db.models import User
 from app.db.repos.user_repo import UserRepository
+from app.db.repos.exceptions import NotFoundError
 from app.core import config
 
 
@@ -14,14 +15,14 @@ def get_current_user(session: Session = Depends(get_db), authorization: str = He
     """Gets the current user from the authorization header."""
     userRepo = UserRepository(session)
 
-    # Debug Only
-    if authorization and config.DEBUG:
-        email = authorization
-        if user := userRepo.getUserByEmail(email):
-            return user
+    try:
+        # Debug Only
+        if authorization and config.DEBUG:
+            email = authorization
+            if user := userRepo.getUserByEmail(email):
+                return user
 
-    if authorization:
-        try:
+        if authorization:
             tokenData = jwt.decode(authorization, config.TOKEN_SECRET, algorithms=['HS256'])
             userId = tokenData.get('user_id')
 
@@ -29,9 +30,9 @@ def get_current_user(session: Session = Depends(get_db), authorization: str = He
                 if user := userRepo.getUser(uuid.UUID(userId)):
                     return user
 
-        except (ValueError, jwt.PyJWTError):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials"
-            )
+    except (NotFoundError, ValueError, jwt.PyJWTError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials"
+        )
 
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User not found")
